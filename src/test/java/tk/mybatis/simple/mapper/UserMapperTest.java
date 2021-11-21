9cb614183a2fcb35854ca650806f53c3920aeff5
ghostchan <jinnchan@qq.com>
2021-11-21 09:46:27 +0800

    p31 单表查询

---
+++ src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
@@ -0,0 +1,50 @@
+package tk.mybatis.simple.mapper;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+import tk.mybatis.simple.model.SysUser;
+
+import java.util.List;
+
+public class UserMapperTest extends BaseMapperTest {
+
+    @Test
+    public void testSelectById() {
+        //获取sqlSession
+        SqlSession sqlSession = getSqlSession();
+        try {
+            //获取UserMapper接口
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            //调用selectById方法，查询id=1的用户
+            SysUser user = userMapper.selectById(1L);
+            //user 不为空
+            Assert.assertNotNull(user);
+            //userName = admin
+            Assert.assertEquals("admin", user.getUserName());
+        } finally {
+            //不要忘记关闭sqlSession
+            sqlSession.close();
+        }
+    }
+
+
+    @Test
+    public void testSelectAll() {
+        //获取sqlSession
+        SqlSession sqlSession = getSqlSession();
+        try {
+            //获取UserMapper接口
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            //调用selectAll方法，查询所有用户
+            List<SysUser> userList = userMapper.selectAll();
+            //user 不为空
+            Assert.assertNotNull(userList);
+            //用户数量大于0个
+            Assert.assertTrue(userList.size() > 0);
+        } finally {
+            //不要忘记关闭sqlSession
+            sqlSession.close();
+        }
+    }
+}
