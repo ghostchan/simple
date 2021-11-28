055f4415187dc6a20104a4333a473339f7965819
ghostchan <jinnchan@qq.com>
2021-11-28 09:53:50 +0800

    p59  @update  @deleted

--- src/test/java/tk/mybatis/simple/mapper/RoleMapperTest.java
+++ src/test/java/tk/mybatis/simple/mapper/RoleMapperTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import tk.mybatis.simple.model.SysRole;
 
+import java.util.Date;
 import java.util.List;
 
 public class RoleMapperTest extends BaseMapperTest {
@@ -54,6 +55,114 @@ public class RoleMapperTest extends BaseMapperTest {
             Assert.assertNotNull(roleList.get(0).getRoleName());
 
         } finally {
+
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void insert() {
+        //获取sqlSession
+        SqlSession sqlSession = getSqlSession();
+        try {
+
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            SysRole role = new SysRole();
+            role.setId(3L);
+            role.setRoleName("测试人员");
+            role.setEnabled(1);
+            role.setCreateBy(1L);
+            role.setCreateTime(new Date());
+
+            roleMapper.insert(role);
+
+        } finally {
+            sqlSession.commit();
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void insert2() {
+        //获取sqlSession
+        SqlSession sqlSession = getSqlSession();
+        try {
+
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            SysRole role = new SysRole();
+            role.setRoleName("开发人员");
+            role.setEnabled(1);
+            role.setCreateBy(1L);
+            role.setCreateTime(new Date());
+
+            roleMapper.insert2(role);
+
+        } finally {
+            sqlSession.commit();
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void insert3() {
+        //获取sqlSession
+        SqlSession sqlSession = getSqlSession();
+        try {
+
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            SysRole role = new SysRole();
+            role.setRoleName("需求人员");
+            role.setEnabled(1);
+            role.setCreateBy(1L);
+            role.setCreateTime(new Date());
+
+            roleMapper.insert3(role);
+
+        } finally {
+            sqlSession.commit();
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+
+    @Test
+    public void updateById() {
+        //获取sqlSession
+        SqlSession sqlSession = getSqlSession();
+        try {
+
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            SysRole role = new SysRole();
+            role.setId(6L);
+            role.setRoleName("需求人员2");
+            role.setEnabled(2);
+            role.setCreateBy(1L);
+            role.setCreateTime(new Date());
+
+            roleMapper.updateById(role);
+
+        } finally {
+            sqlSession.commit();
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void deleteById() {
+        //获取sqlSession
+        SqlSession sqlSession = getSqlSession();
+        try {
+
+            RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+            roleMapper.deleteById(6L);
+
+        } finally {
+            sqlSession.commit();
             //不要忘记关闭 sqlSession
             sqlSession.close();
         }
